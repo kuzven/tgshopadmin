907f6c2c100a3f7c5b3f6368ffd5315b38c6d19f
kuzven <kuzven@gmail.com>
2025-06-02 13:41:16 +0400

    Added model to faq application and registered in the admin area

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=255, verbose_name="Вопрос")
+    answer = models.TextField(verbose_name="Ответ")
+    
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Вопрос FAQ"
+        verbose_name_plural = "Вопросы FAQ"
